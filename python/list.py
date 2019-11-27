e38a2df4c3473bd13763922f63e69c2288205a22
reski <pesan.reski@gmail.com>
2019-11-27 20:27:43 +0700

    pyton list

---
+++ python/list.py
@@ -0,0 +1,7 @@
+animal=["Duck","Chicken","Dog","Cow","Rat","Rabbit","Horse"]
+
+cartoon=["Spongebob","Rick and Morty","Jhonny English","Tom and Jerry"]
+
+belanjaan = ["kopi","susu","rice","lipstick"]
+
+print(animal [-1::-2])
