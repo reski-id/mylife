b4b8d04c1cf23d85ade9bf2f182d179701003fde
reski <pesan.reski@gmail.com>
2019-11-29 14:11:41 +0700

    function

--- python/func.py
+++ python/func.py
@@ -4,6 +4,6 @@ age = input("your age is : ")
 
 
 def hello(name, age):
-    return "hello {} you are {} years old".format(name,age)
+    return "hello {} you are {} years old\nHave a Great Day".format(name,age)
 
 print(hello(name,age))
