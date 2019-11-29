7e3d8757289a5568d578f3b592e08704fa62ab72
reski <pesan.reski@gmail.com>
2019-11-29 13:16:38 +0700

    func update

--- python/func.py
+++ python/func.py
@@ -1,7 +1,7 @@
 def hello(name, age):
     return "hello {} you are {} years old".format(name,age)
 
-print(hello("Reski",34))
+print(hello("Jean",25))
 
 def penjumlahan (x,y):
     return x+y
@@ -16,7 +16,6 @@ def perkalian (x,y):
     return x*y
 
 
-print(penjumlahan(5,10))
 print(pengurangan(15,10))
 print(pembagian(15,5))
 print(perkalian(5,10))
