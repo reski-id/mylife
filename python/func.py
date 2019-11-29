c85eca70fc1aa9b1db8c1c1519563ffbfa885203
reski <pesan.reski@gmail.com>
2019-11-29 14:02:26 +0700

    add input

--- python/func.py
+++ python/func.py
@@ -1,21 +1,9 @@
+#print name and age from user
+name = input("your name is : ")
+age = input("your age is : ")
+
+
 def hello(name, age):
     return "hello {} you are {} years old".format(name,age)
 
-print(hello("Jean",25))
-
-def penjumlahan (x,y):
-    return x+y
-
-def pengurangan (x,y):
-    return x-y
-
-def pembagian (x,y):
-    return x/y
-
-def perkalian (x,y):
-    return x*y
-
-
-print(pengurangan(15,10))
-print(pembagian(15,5))
-print(perkalian(5,10))
+print(hello(name,age))
