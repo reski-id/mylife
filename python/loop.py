84804c727d4086e9910f86da7c1a4202740c07fd
reski <pesan.reski@gmail.com>
2019-11-30 17:46:16 +0700

    loop segi empat

---
+++ python/loop.py
@@ -0,0 +1,10 @@
+# menammpilkan bintang segi empat dengan inputan user
+ 
+sisi = int(input("Masukkan Angka  : "))
+
+print("Segi Empat") 
+
+for i in range(sisi):
+    for i in range(sisi):
+        print('*', end = '  ')
+    print()
