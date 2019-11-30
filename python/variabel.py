b947c3955e7612fea3e816b6193d349c508f9af6
reski <pesan.reski@gmail.com>
2019-11-30 17:59:33 +0700

    add variabel

--- python/variabel.py
+++ python/variabel.py
@@ -1,8 +1,18 @@
-age = 25
+print("Masukkan Nama dan Umur")
+print(10*"-")
+name = input("Masukkan Nama :")
+age = input("Masukkan Umur :")
 
-name = "Reski"
+print("hello  {} umur anda adalah {} Tahun".format(name,age))
 
-print("hello Nama saya adalah {} dan saya {} Tahun".format(name,age))
+print("Masukkan nominal harga dan Jumlah Bayar")
+print(20*"-")
+nominal = int(input("Masukkan nominal harga:"))
+bayar = int(input("Masukkan Jumlah Bayar :"))
+
+sisa = (nominal-bayar)
+
+print("Sisa =",sisa)
 
 # satu baris komentar dalam python
 
