8e7e524a4df04fc39a08156f7664c4c4c3421bc8
reski <pesan.reski@gmail.com>
2019-11-30 18:06:56 +0700

    variabel update

--- python/variabel.py
+++ python/variabel.py
@@ -1,10 +1,14 @@
+# Mengambil data nama dan umur dari user dan menampilkannya di console
 print("Masukkan Nama dan Umur")
 print(10*"-")
+
 name = input("Masukkan Nama :")
 age = input("Masukkan Umur :")
 
 print("hello  {} umur anda adalah {} Tahun".format(name,age))
 
+
+# Mengambil data nnominal bayar dan harga barang  dari user dan menampilkannya dalam bentuk sisa bayar di console
 print("Masukkan nominal harga dan Jumlah Bayar")
 print(20*"-")
 nominal = int(input("Masukkan nominal harga:"))
