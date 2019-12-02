eaa67a5696db9443ec26778882711c5803ee13b2
reski <pesan.reski@gmail.com>
2019-12-02 15:28:02 +0700

    update

--- python/ifelse.py
+++ python/ifelse.py
@@ -1,31 +1,36 @@
-age = 25
+age = 55
 
-name = "Reski"
+name = "Dr Grigory House"
 
-if(age>=25):
-    print ("lebih dari 25 tahun")
-    print ("atau sama dengan 25")
+#jika umur lebih dari 25 print lebih dari 25
+
+if(age>=18):
+    print ("lebih dari 18 tahun")
+    print ("Kamu bisa mendapatkan Licence")
 else:
     print ("umur anda")
-    print ("kurang dari 25 tahun")
+    print ("Kamu belum bisa mendapatkan Licence")
 
-if(name=="Reski"):
-    print ("Hai Reski")
+if(name=="Dr Grigory House"):
+    print ("Hai Doctor")
 else:
     print ("Hello ?")
 
+#jika cuaca cerah => ayo main
 cuacacerah= False
 if(cuacacerah):
     print ("let's go main")
 else:
      print ("Cuaca tidak cerah")
 
+#jika bilangan lebih besar atau kecil dari 4
 x=5
 if x<4:
     print ("kecil dari 4")
 else:
     print ("besar dari 4")
 
+#tentukan bilangan ganjil dan genap
 angka = int(input("Masukkan Angka :"))
 if angka % 2 is 0:
     print("Bilangan Genap")
