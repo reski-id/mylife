218d8dad7c0bc3a31bb65ff0872f8ccf5861b453
reski <pesan.reski@gmail.com>
2019-12-05 21:52:51 +0700

    python

--- python/number_note.py
+++ python/number_note.py
@@ -1,31 +1,4 @@
-#####################################
-#### Numbers and more in Python! ####
-#####################################
 
-#
-#     1.) Types of Numbers in Python
-#     2.) Basic Arithmetic
-#     3.) Differences between Python 2 vs 3 in division
-#     4.) Object Assignment in Python
-
-# Types of numbers
-#
-# Python has various "types" of numbers (numeric literals). We'll mainly focus on
-# integers and floating point numbers.
-#
-# Integers are just whole numbers, positive or negative. For example: 2 and -2 are
-#  examples of integers.
-# 
-# Floating point numbers in Python are notable because they have a decimal point
-# in them, or use an exponential (e) to define the number. For example 2.0 and -2.1
-# are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is
-# also an example of a floating point number in Python.
-#
-# Throughout this course we will be mainly working with
-# integers or simple float number types.
-
-
-# Dasar Aritmatika
 # Penjumlahan
 2+1
 
@@ -52,23 +25,13 @@
 
 
 ## Variable Assignments
-#
-# Now that we've seen how to use numbers in Python as a calculator let's see how
-# we can assign names and create variables.
-#
-# We use a single equals sign to assign labels to variables.
-# You also don't need to specify the keyword var.
-# Let's see a few examples of how we can do this.
 
 # Let's create an object called "a" and assign it the number 5
 a = 5
 
-# Now if I call a in my Python script, Python will treat it as the number 5.
-
 # Adding the objects
 a+a
 
-# What happens on reassignment? Will Python let us write it over?
 
 # Reassignment
 a = 10
